2fdb4a7505a2d17eead1701eba7000d87005435f
limwantae <apts2@naver.com>
2023-05-31 13:56:55 +0900

    start

---
+++ src/main/java/com/main/SQLInjectionExample.java
@@ -0,0 +1,18 @@
+package com.main;
+
+public class SQLInjectionExample {
+        public static void main(String[] args) {
+            String username = getUserInput();
+            String query = "SELECT * FROM users WHERE username = '" + username + "'";
+            // query 실행 로직
+        }
+<<<<<<< HEAD
+   
+=======
+
+>>>>>>> parent of 582cfa6 (commit 1)
+        private static String getUserInput() {
+            // 사용자로부터 입력 받는 로직
+            return "TEST";
+        }
+    }
